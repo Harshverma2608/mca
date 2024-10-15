c3035c62162f7b23b0ebce3545c03e1c3e1910ad
Harshverma2608 <harshkumarverma26@gmail.com>
2024-10-15 11:29:59 +0530

    adding new files

---
+++ python/lab1/Introduction/cfile.py
@@ -0,0 +1,14 @@
+# python program to find square root
+
+import math
+
+num = int(input("enter the value: "))
+srt = math.sqrt(num)
+
+print("the square root of the number is : ", srt)
+
+# Another program
+
+# num = int(input("enter the value "))
+# srt = num ** 0.5
+# print("result is : ", srt)
